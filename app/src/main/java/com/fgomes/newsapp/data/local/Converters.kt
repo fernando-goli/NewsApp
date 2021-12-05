81558671d24678fd07056f141e4b21523e1276ed
fernando gomes <fernando.gmsoli@gmail.com>
2021-12-04 21:14:12 -0300

    feat: create instance retrofit e room database and models

---
+++ app/src/main/java/com/fgomes/newsapp/data/local/Converters.kt
@@ -0,0 +1,20 @@
+package com.fgomes.newsapp.data.local
+
+import androidx.room.TypeConverter
+import com.fgomes.newsapp.data.model.Source
+
+class Converters {
+    /**
+     * Conversor de tipo, conversor é criado para Room interpretar a classe e
+     * converter tipos que não sejam primitivos ou string, ex: class Source
+     * */
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+}
---
+++ app/src/main/java/com/fgomes/newsapp/data/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.fgomes.newsapp.data.util
+
+object Constants {
+    const val API_KEY = "00d632aa0d424911af2b8508f87de764"
+    const val BASE_URL = "https://newsapi.org"
+}

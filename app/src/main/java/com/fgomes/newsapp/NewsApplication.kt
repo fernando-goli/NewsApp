d78eeae46829538f8408d729ffb9f660e4236544
fernando gomes <fernando.gmsoli@gmail.com>
2021-12-06 20:20:14 -0300

    add application and utils

---
+++ app/src/main/java/com/fgomes/newsapp/NewsApplication.kt
@@ -0,0 +1,5 @@
+package com.fgomes.newsapp
+
+import android.app.Application
+
+class NewsApplication : Application()
--- app/src/main/java/com/fgomes/newsapp/util/Constants.kt
+++ app/src/main/java/com/fgomes/newsapp/util/Constants.kt
@@ -1,6 +1,8 @@
-package com.fgomes.newsapp.data.util
+package com.fgomes.newsapp.util
 
 object Constants {
     const val API_KEY = "00d632aa0d424911af2b8508f87de764"
     const val BASE_URL = "https://newsapi.org"
+    const val SEARCH_NEWS_TIME_DELAY = 500L
+    const val QUERY_PAGE_SIZE = 20
 }

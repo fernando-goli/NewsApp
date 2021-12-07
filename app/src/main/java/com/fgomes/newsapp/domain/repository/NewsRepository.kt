8b3bf167fd741d5e6861c332488f254c0ceeea88
fernando gomes <fernando.gmsoli@gmail.com>
2021-12-06 22:21:11 -0300

    create NewsRepository

---
+++ app/src/main/java/com/fgomes/newsapp/domain/repository/NewsRepository.kt
@@ -0,0 +1,22 @@
+package com.fgomes.newsapp.domain.repository
+
+import com.fgomes.newsapp.data.local.ArticleDatabase
+import com.fgomes.newsapp.data.models.Article
+
+
+class NewsRepository(
+    val db: ArticleDatabase
+) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
+
+}
